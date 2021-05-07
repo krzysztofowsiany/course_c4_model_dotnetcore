b257180a07053ef5296357fb5dfcd58e34935a18
Krzysztof Owsiany <gocom@o2.pl>
2021-05-07 21:23:15 +0200

    arch: Change payment provider from TPay to Stripe

---
+++ ADL/adr-5-change-tpay-to-stripe.md
@@ -0,0 +1,19 @@
+# Architecture Decision Record: Change payment provider from TPay to Stripe.
+
+## Status
+
+accepted
+
+## **Context**
+
+We have some problem with TPay. On BigPicture we decide to change payment provider.
+
+## Decision
+
+Use Stripe like payment provider. 
+Use Stripe.NET library to handle payments.
+
+## Consequences
+
+We have to remove Payment Controller and use Stripe.NET.
+The implementation new feature will be faster.
--- Layers/C1.cs
+++ Layers/C1.cs
@@ -7,16 +7,16 @@ namespace modelc4_project.Layers {
                 .AddPerson("Admin", "Configuring platform, generating coupons")
                 .AddPerson("Customer", "Using course platform, buy on platform")
                 .AddSoftwareSystem("Course Platform", "Sell and delivery courses to customers")
-                .AddSoftwareSystem("TPay", "Handle payments", "External")
+                .AddSoftwareSystem("Stripe", "Handle payments", "External")
                 .AddSoftwareSystem("iFirma", "Generate invoices", "External")
                 .AddSoftwareSystem("SMTP", "Send emails", "External")
                 .PersonUses("Customer", "Course Platform", "Buy and pass the courses")
                 .PersonUses("Admin", "Course Platform", "Manages")
-                .SoftwareSystemUses("Course Platform", "TPay", "Request payments")
+                .SoftwareSystemUses("Course Platform", "Stripe", "Request payments")
                 .SoftwareSystemUses("Course Platform", "iFirma", "Generate invoices")
                 .SoftwareSystemUses("Course Platform", "SMTP", "Send emails")
                 .SoftwareSystemDelivers("SMTP", "Customer", "Send emails")
-                .SoftwareSystemUses("TPay", "Course Platform", "Responses")
+                .SoftwareSystemUses("Stripe", "Course Platform", "Responses")
                 .AddStyle(new ElementStyle(Tags.SoftwareSystem) {
                     FontSize = 30,
                         Background = "#FB9D4B"
